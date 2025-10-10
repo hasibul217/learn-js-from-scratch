5f97c5c72c27ea6d854c014d67a3b4b80fc8d5b3
hasibul217 <shantossnic@gmail.com>
2025-10-10 08:27:13 +0600

    day-02

---
+++ day-02/practice-01/index.js
@@ -0,0 +1,18 @@
+let studentName = "Anderson Silva"
+let studentAge = 20 
+let isStudent = true 
+let favouriteLanguage = "Javascript"
+
+
+studentName = "GSP"
+
+console.log("Student name: ", studentName)
+console.log("Student Age: ", studentAge )
+console.log("Student Identity: ", isStudent)
+console.log("Favourite Programming language: ", favouriteLanguage)
+
+
+
+
+//index.js:7 Uncaught TypeError: Assignment to constant variable (if i use const instead of let) 
+  
---
+++ day-02/practice-02/index.js
@@ -0,0 +1,13 @@
+studentObject = {
+    studentName : "Mr. man",
+    studentAge : 12,
+    status: true,
+    studentParentName : "Mota",
+    studentMark : null
+
+}
+
+
+console.log ("Name: ", studentObject.studentAge)
+
+// console.log ("Guardian Name: ", studentObject.studentParentName)
