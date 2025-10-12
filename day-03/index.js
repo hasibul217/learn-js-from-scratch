21fab1ae6e03aa3d7e85d2275ea96c6d763e478e
hasibul217 <shantossnic@gmail.com>
2025-10-12 09:30:52 +0600

    added

---
+++ day-03/index.js
@@ -0,0 +1,25 @@
+let firstWord = "Hello";
+
+let secondWord = "Bangladesh";
+
+let fullSentence = firstWord +(" ")+ secondWord;
+
+// console.log (firstWord + secondWord)
+
+console.log(fullSentence);
+
+document.getElementById("someText").innerText = (fullSentence);
+
+// document.getElementById("someText").document.write() = (fullSentence)
+
+console.log ("***Comparison Operators***")
+
+console.log ('3' == 3)
+console.log (0 == false )
+
+
+console.log ('3' === 3)
+console.log (0 === false )
+
+
+console.log ("***Logical Operators***")
---
+++ day-03/practice/practice.js
@@ -0,0 +1,12 @@
+console.log("**These are the practice**");
+
+console.log("*Practice 01*");
+
+let x = 23;
+
+console.log("Given number :", x);
+
+if (x / 2 == 0) console.log("Even Number");
+else console.log("Odd Number");
+
+console.log("*Practice 02*")
