021e005c55fc4b2da66972c5e07cea9ba430f68f
hasibul217 <shantossnic@gmail.com>
2025-11-24 07:58:12 +0600

    done

---
+++ day-13/practiceThis.js
@@ -0,0 +1,11 @@
+console.log("Practicing This keyword")
+
+//task 1
+const fighter = {
+    name : "Khabib",
+    sayName (){
+        return (`The player name is ${this.name}.`)
+    }
+}
+
+console.log(fighter.sayName());
