fc4fa2ccd25f161d91c7397f66c0470596af00e8
hasibul217 <shantossnic@gmail.com>
2025-12-30 22:35:10 +0600

    added

---
+++ day-15-array/array.js
@@ -0,0 +1,31 @@
+const lightWeight = ["Ilia", "Arman", "Paddy", "Chanko"];
+
+const walterWeight = new Array ("Islam", "Carlos", "Shavkat", "Jack");
+
+const middleWeight = [];
+
+console.log(lightWeight);
+console.log(walterWeight);
+console.log(middleWeight);
+
+//task 1.2 
+const champions = ["Khabib", "Islam", "Alexander", "Leon", "Jon"];
+
+console.log(`First champion is ${champions[0]}`);
+console.log(`Third champion is ${champions[2]}`);
+console.log(`Last champion is ${champions[4]}`);
+
+console.log(`Total number of champion is ${champions.length}.`)
+
+//task 1.3 
+
+const roster = ["Khabib", "Islam", "Conor", "Dustin", "Charles"];
+
+// 1. Log the length of the roster
+console.log (`The length of the roster is ${roster.length}`)
+// 2. Access the LAST element using length (Hint: length - 1)
+console.log ( `The last number of element is ${roster[roster.length-1]} `)
+// 3. Log how many fighters are in the roster
+console.log(`Total number of fighter in the roster is ${roster.length}.`)
+
+//Level two 
