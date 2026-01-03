a42154f621854bc7263b85f464da2310682ae602
hasibul217 <shantossnic@gmail.com>
2026-01-03 23:39:09 +0600

    solved

---
+++ day-15-array/plus-minus.js
@@ -0,0 +1,42 @@
+const testArray = [2, 8, 0, -5, -9, 0, 5, 5];
+
+const arrayLength = testArray.length;
+
+console.log(arrayLength);
+
+// const positiveRatio = () => {
+
+//     const posArray = testArray.filter (numbers => numbers > 0)
+//     const ratio = posArray.length / arrayLength;
+//     console.log(`Positive Ratio : ${ratio.toFixed(6)}`)
+
+// }
+
+// const negativeRatio = () => {
+
+//     const negArray = testArray.filter (numbers => numbers < 0)
+//     const ratio = negArray.length / arrayLength;
+//     console.log(`Negative Ratio : ${ratio.toFixed(6)}`)
+
+// }
+
+// const zeroRatio = () => {
+
+//     const zeroArray = testArray.filter (number => number === 0)
+//     const ratio =  zeroArray.length / arrayLength;
+//     console.log(`Zero ratio : ${ratio.toFixed(6)}`)
+
+
+// }
+
+
+
+
+
+
+
+
+
+positiveRatio();
+negativeRatio();
+zeroRatio();
