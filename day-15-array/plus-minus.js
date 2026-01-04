87ea761b9cd85850dfe362046a6cb2397a7d658b
hasibul217 <shantossnic@gmail.com>
2026-01-04 23:51:16 +0600

    reduce method

--- day-15-array/plus-minus.js
+++ day-15-array/plus-minus.js
@@ -4,30 +4,30 @@ const arrayLength = testArray.length;
 
 console.log(arrayLength);
 
-// const positiveRatio = () => {
+const positiveRatio = () => {
 
-//     const posArray = testArray.filter (numbers => numbers > 0)
-//     const ratio = posArray.length / arrayLength;
-//     console.log(`Positive Ratio : ${ratio.toFixed(6)}`)
+    const posArray = testArray.filter (numbers => numbers > 0)
+    const ratio = posArray.length / arrayLength;
+    console.log(`Positive Ratio : ${ratio.toFixed(6)}`)
 
-// }
+}
 
-// const negativeRatio = () => {
+const negativeRatio = () => {
 
-//     const negArray = testArray.filter (numbers => numbers < 0)
-//     const ratio = negArray.length / arrayLength;
-//     console.log(`Negative Ratio : ${ratio.toFixed(6)}`)
+    const negArray = testArray.filter (numbers => numbers < 0)
+    const ratio = negArray.length / arrayLength;
+    console.log(`Negative Ratio : ${ratio.toFixed(6)}`)
 
-// }
+}
 
-// const zeroRatio = () => {
+const zeroRatio = () => {
 
-//     const zeroArray = testArray.filter (number => number === 0)
-//     const ratio =  zeroArray.length / arrayLength;
-//     console.log(`Zero ratio : ${ratio.toFixed(6)}`)
+    const zeroArray = testArray.filter (number => number === 0)
+    const ratio =  zeroArray.length / arrayLength;
+    console.log(`Zero ratio : ${ratio.toFixed(6)}`)
 
 
-// }
+}
 
 
 
